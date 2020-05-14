597bf39c19e1ec1dd6e56768e44a5b145e46f002
Usep Syaiful Hidayat <58686569+usepsh@users.noreply.github.com>
2020-05-14 13:03:24 +0700

    pertemuan12
    menambahkan fitur login dan registrasi

---
+++ kuliah/pertemuan12/detail.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+require 'functions.php';
+$id = $_GET['id'];
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>"></li>
+    <li>NRP : <?= $m['nrp']; ?></li>
+    <li>NAMA : <?= $m['nama']; ?></li>
+    <li>EMAIL : <?= $m['email']; ?></li>
+    <li>JURUSAN : : <?= $m['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $m['id']; ?>">Ubah</a> | <a href="hapus.php?id=<?= $m['id']; ?>" onclick="return confirm('Apakah anda yakin ingin menghapus data?')">hapus</a></li>
+    <li><a href="index.php">Kembali ke daftar mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
